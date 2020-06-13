39d859232d16514579e6d7f57ae1c2cc0cd74f36
Yesaswi Vadapalli <yesaswi.vadapalli@gmail.com>
2020-06-13 16:23:36 +0530

    Pop and Push Digits & Check before Overflow Approach

--- Easy/reverse-integer.cpp
+++ Easy/reverse-integer.cpp
@@ -36,3 +36,20 @@ public:
         return 0;
     }
 };
+
+// Optimized Approach 
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+};
